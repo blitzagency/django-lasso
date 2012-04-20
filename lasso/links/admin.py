7a5582572109774d9a9429ab9eda83f88b63334d
Dino Petrone <dinopetrone@gmail.com>
2012-04-20 15:53:08 -0700

    stuff

--- lasso/links/admin.py
+++ lasso/links/admin.py
@@ -5,10 +5,11 @@ from django.contrib import admin
 
 
 class TagInline(admin.StackedInline):
-    model = Tag
-    extra = 3
+	model = Tag
+	extra = 3
 
 class LinkAdmin(admin.ModelAdmin):
-    inlines = [TagInline]
+	list_display = ('url', 'pub_date')
+	inlines = [TagInline]
 
 admin.site.register(Link, LinkAdmin)
